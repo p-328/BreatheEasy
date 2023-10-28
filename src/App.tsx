5831e0bf0df47c542b45574894299c750fc218e8
Toby Nguyen <nightilon.personal@gmail.com>
2023-10-28 17:13:29 -0500

    Get user location

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState('');
+
+  // Function to get and store the user's location
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      setLocation("Geolocation is not supported by this browser.");
+    }
+  }
+
+  // Function to display the user's location
+  function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setLocation(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  }
+
+  // Use useEffect to call getLocation when the component mounts
+  useEffect(() => {
+    getLocation();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>User Location</h1>
+      <p>{location}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
