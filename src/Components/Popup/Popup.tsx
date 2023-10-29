30cfb4e3d0912ec46d85cdfb4838660385154f57
p-328 <69773020+p-328@users.noreply.github.com>
2023-10-28 23:43:20 -0500

    Updated Popup.tsx

--- src/Components/Popup/Popup.tsx
+++ src/Components/Popup/Popup.tsx
@@ -1,27 +1,43 @@
 import { useState, useEffect } from 'react';
-
 export const Popup = () => {
-	const [medicalCondition, setMedicalCondition] = useState(false);
+	const [medicalCondition, setMedicalCondition] = useState("");
 	const [clicked, setClicked] = useState(false);
 	useEffect(() => {
-		if (localStorage.getItem("medicalCondition") == "true") 
-			setMedicalCondition(true);
-	}, [medicalCondition]);
-	useEffect(() => {
-		if (localStorage.getItem("isClicked") == "true") {
-			setClicked(true);
-		}
+		setMedicalCondition(localStorage.getItem("medicalCondition"));
 	}, []);
-	function handlePopupClick(e) {
+	useEffect(() => {
+		setClicked(localStorage.getItem("isClicked") == true);
+	}, []);
+	function handleYes(e) {
 		e.preventDefault();
-		setClicked(true);
 		localStorage.setItem("isClicked", "true");
+		setClicked(true);
+		localStorage.setItem("medicalCondition", "Yes");
+		setMedicalCondition("Yes");
 	} 
+	function handleNo(e) {
+		e.preventDefault();
+		localStorage.setItem("isClicked", "true");
+		setClicked(true);
+		localStorage.setItem("medicalCondition", "No");
+		setMedicalCondition("No");
+	}
 	return (
 		<div>
-			{clicked ? <div></div> : <button onClick={handlePopupClick}>
-			 	click me
-			</button>}
+			{
+				!clicked ?  
+					<div>
+						<p>Do you have a medical condition that is associated with respiratory issues (pneumonia, bronchitis, etc.)?</p>
+						<br />
+						<button onClick={handleYes}>
+						 	Yes
+						</button>
+						<button onClick={handleNo}>
+							No
+						</button>
+					</div>
+					: <div></div> 
+			}
 		</div>
-	);
+	)
 }
