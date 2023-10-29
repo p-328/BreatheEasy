e4e0cccb9f7210aadcb9a65ff024b5c0e4ae13ee
Toby Nguyen <nightilon.personal@gmail.com>
2023-10-28 22:14:25 -0500

    Implement algorithm to fill missing parameters with next closest sensor

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,28 @@ function App() {
     setLocation(locationString);
   }
 
+  // Custom sorting function to sort parameter names correctly
+  function customSort(a, b) {
+    const nameA = a.parameter.toLowerCase();
+    const nameB = b.parameter.toLowerCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // If the names are the same, compare numerically
+    const numA = parseInt(a.parameter.match(/\d+/));
+    const numB = parseInt(b.parameter.match(/\d+/));
+
+    return numA - numB;
+  }
+
   // Function to make the API request
   function makeApiRequest(location) {
-    const apiUrl = `https://api.openaq.org/v2/locations?limit=1&page=1&offset=0&sort=desc&coordinates=${encodeURIComponent(location)}&radius=25000&order_by=distance&dump_raw=false`;
+    const apiUrl = `https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&coordinates=${encodeURIComponent(location)}&radius=25000&order_by=distance&dump_raw=false`;
 
     fetch(apiUrl, {
       method: 'GET',
@@ -36,19 +55,26 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         if (data.results && data.results.length > 0) {
-          const locationData = data.results[0];
-          const name = locationData.name;
-          // Filter parameters to exclude humidity, temperature, and pressure
-          const parameters = locationData.parameters.filter(
-            (param) =>
-              param.parameter !== 'humidity' &&
-              param.parameter !== 'temperature' &&
-              param.parameter !== 'pressure'
-          );
-          const parametersInfo = parameters.map(
-            (param) => `${param.parameter}: ${param.count} ${param.unit}`
-          );
-          setApiResponse(`Name: ${name}\nParameters:\n${parametersInfo.join('\n')}`);
+          const parametersSeen = new Set();
+          let parametersInfo = [];
+          data.results.forEach((locationData) => {
+            const name = locationData.name;
+            const sortedParameters = locationData.parameters
+              .filter((param) =>
+                param.parameter !== 'humidity' &&
+                param.parameter !== 'temperature' &&
+                param.parameter !== 'pressure' &&
+                !parametersSeen.has(param.parameter)
+              )
+              .sort(customSort); // Sort the parameters using the custom sorting function
+
+            sortedParameters.forEach((param) => {
+              parametersInfo.push(`${name}: ${param.parameter}: ${param.count} ${param.unit}`);
+              parametersSeen.add(param.parameter);
+            });
+          });
+
+          setApiResponse(`Parameters:\n${parametersInfo.join('\n')}`);
         } else {
           setApiResponse('Location data not found.');
         }
